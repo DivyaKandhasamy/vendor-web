1bce7c9d8c50282c6e039b492590ad8fac483bf2
Loknath Kumar Mishra <loknath.mishra@framsikt.no>
2024-08-04 19:30:16 +0530

    axios setup

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,48 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Analytics from '../components/Analytics';
 import Cards from '../components/Cards';
 import Hero from '../components/Hero';
 import Newsletter from '../components/Newsletter';
 import Grid from '../components/Grid'
-
+import axios from 'axios'
 function Home() {
+  const [gridData, setGridData] = useState([]);
+  const [gridColumns, setGridColumns] = useState([]);
+  useEffect(() => {
+    const getData = async () => {
+      try{
+        const url='http://localhost:5000/vendors'
+        const response = await axios.get(url);
+        setGridData(response.data);
+        setGridColumns([{
+          field: 'vendorName'
+        },
+        {
+          field: 'vendorDesc'
+        },
+        {
+          field: 'breachDate'
+        },
+        {
+          field: 'dateAdded'
+        },
+        {
+          field: 'compromisedAccounts'
+        },
+        {
+          field: 'compromisedData'
+        },
+        {
+          field: 'riskScore'
+        }]);
+      }
+      catch (error){
+        console.log(error);
+      }
+    }
+    getData();
+  },[])
+
   return (
     <div>
       <div>
