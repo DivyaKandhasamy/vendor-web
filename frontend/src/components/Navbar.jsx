8155dfa8961d4424ed6f52b35183ade1dfcf59ef
Divya <divyak192001@gmail.com>
2024-08-12 22:05:50 +0530

    navbar changes

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai';
 import logo from '../assets/darknet.png';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -48,16 +48,13 @@ const Navbar = () => {
             APT
           </NavLink>
         </li>
-        <li key={0} className='p-4'><Link to="/">Home</Link></li>
-        <li key={1} className='p-4'><Link to="/vendors">Vendors</Link></li>
-        <li key={2} className='p-4'><Link to="/apt">APT</Link></li>
         <button className='bg-[#00df9a] hover:brightness-75 ease-in duration-150 cursor-pointer w-[150px] rounded-md font-medium my-6 mx-auto py-2 text-black'>Login</button>
       </ul>
       <div onClick={handleNav} className='block md:hidden'>
         {nav ? <AiOutlineClose size={20}/> : <AiOutlineMenu size={20} />}
       </div>
       <ul className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500' : 'ease-in-out duration-500 fixed left-[-100%]'}>
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>Darknet Safe</h1>
         <li className='p-4 border-b border-gray-600'>
           <NavLink
             to="/"
@@ -89,13 +86,6 @@ const Navbar = () => {
             APT
           </NavLink>
         </li>
-        <li className='p-4 border-b border-gray-600'>About</li>
-        <li className='p-4'>Contact</li>
-          <li key={0} className='p-4 border-b border-gray-600'>Home</li>
-          <li key={1} className='p-4 border-b border-gray-600'>Company</li>
-          <li key={2} className='p-4 border-b border-gray-600'>Resources</li>
-          <li key={3} className='p-4 border-b border-gray-600'>About</li>
-          <li key={4} className='p-4'>Contact</li>
       </ul>
     </div>
   );
